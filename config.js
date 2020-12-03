66a40ee219ee6cb4c6a756507c3ac8700fdbc2e3
ajaymehul <ajaymehul2013gmail.com>
2020-12-03 08:17:51 -0600

    shift request for specific user

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 module.exports.uri = "mongodb+srv://ajay:pass495@cluster0.02djz.mongodb.net/employees?retryWrites=true&w=majority";
-module.exports.port = 3003; 
+module.exports.port = 3004; 
+
--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -35,4 +35,22 @@ router.get('/shifts', (req, res) => {
     .catch(err => {console.log(err);});
 });
 
+router.get('/shifts/:username', (req, res) => {
+    var employee_username = req.params.username
+    const filter = {assignedTo: {$in: [employee_username]}};
+    Shift.find(filter).then(data => {
+       res.json(data);
+    })
+    .catch(err => {console.log(err);});
+});
+
+router.get('/shiftsOther/:username', (req, res) => {
+    var employee_username = req.params.username
+    const filter = {assignedTo: {$nin: [employee_username]}};
+    Shift.find(filter).then(data => {
+       res.json(data);
+    })
+    .catch(err => {console.log(err);});
+});
+
 module.exports = router;
